35109395473f849f73f017ce175eefa2e2a96cad
H1S0K44 <edwards.noam@gmail.com>
2024-11-29 19:50:44 +0100

    AVL en balle

--- Test.c
+++ Test.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+typedef struct AVL{
+    int stationID; //Id de la station
+    int Capacity ; //Capacité de la station
+    int SumCons ;//Somme de la consomation
+    struct AVL* fright ;
+    struct AVL* fleft ;
+}AVL;
 
-int main(){
-    printf("Je suis Noam le dominant ULTIME");
-    return 0;
+typedef AVL* pAVL;
+
+pAVL createAVL(int station, int Cap, int Sum){
+    pAVL new = malloc(sizeof(AVL));
+    if(new == NULL){
+        exit(EXIT_FAILURE);
+    }
+    new->stationID = station;
+    new->Capacity = Cap;
+    new->SumCons = Sum;
+    return new;
 }
+
